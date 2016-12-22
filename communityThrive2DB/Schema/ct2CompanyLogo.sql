3878d30ae9ecae5ac180b99638ad9f036bfefd6a
xofdub <mattspezia@gmail.com>
2016-12-22 14:45:52 -0600

    Adding companyLogo to Application
    Adding Company Logo table and read stored procedure.

---
+++ communityThrive2DB/Schema/ct2CompanyLogo.sql
@@ -0,0 +1,18 @@
+﻿/*
+Author: Matt Spezia
+Date: 12/21/2016
+Description: Company Logo Table
+*/
+
+
+
+
+
+CREATE TABLE ct2CompanyLogo
+(
+companyLogoID INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+companyIDFK INT FOREIGN KEY REFERENCES ct2company(companyID) NOT NULL,
+companyLogo VARBINARY(MAX) NOT NULL,
+dateAdded DATETIME DEFAULT (GETDATE()),
+isDeleted BIT DEFAULT (0)
+)
