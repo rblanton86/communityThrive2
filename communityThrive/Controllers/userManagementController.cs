a9b877565af45216ba41f93d9851acdec8699702
ebowen8503 <ebowen8503@gmail.com>
2016-12-23 13:52:19 -0600

    created userManagement controller
    moved all user related files into userManagement folder under views,
    created folder called classes

---
+++ communityThrive/Controllers/userManagementController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using communityThrive2.DataControllers;
+using communityThrive2.Models;
+
+namespace communityThrive2.Controllers
+{
+    public class userManagementController : Controller
+    {
+        // GET: userLogin
+        public ActionResult userLogin()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Login(ct2UserDataController currentUser)
+        { 
+             
+            if(currentUser == null) 
+            { 
+                return RedirectToAction("LoggedIn");
+            }
+            else
+            {
+                return View("userLogin");
+            }
+
+
+        }
+
+
+    } 
+    
+}
